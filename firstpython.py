f706f457ad203478b15d2d608920190eeb6a7be0
Gowtham <76685953+gowtham949@users.noreply.github.com>
2023-10-26 20:06:54 +0530

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,10 @@
+def simple_interest(p,t,r):
+p = int(input("Enter the principle amoount:")
+t = int(input("Enter the time period:")
+r = int(input("Enter the interest rate:")
+        simpe_interest(p,t,r)
+        si = (p*t*r)/100
+print("the principle amount", p)
+print("the time period", t)
+print("the interest rate", r)
+print("the simple interest is", si)
